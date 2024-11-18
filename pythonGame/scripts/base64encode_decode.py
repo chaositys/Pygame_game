05b53f44ee3b81603830303b66f3b1b9c3664c41
chaositys <120270595+chaositys@users.noreply.github.com>
2024-11-18 10:17:49 +0000

    Add files via upload

---
+++ pythonGame/scripts/base64encode_decode.py
@@ -0,0 +1,56 @@
+import base64
+
+def encode(number):
+    encoded_bytes = base64.b64encode(number.to_bytes((number.bit_length() + 7) // 8, byteorder='big'))
+    encoded_string = encoded_bytes.decode('utf-8')
+    return encoded_string
+
+def decode(encoded_string):
+    decoded_bytes = base64.b64decode(encoded_string.encode('utf-8'))
+    decoded_number = int.from_bytes(decoded_bytes, byteorder='big')
+    return decoded_number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
